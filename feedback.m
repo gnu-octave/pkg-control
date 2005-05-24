dfc2a7553f7cb2f8fca8cca2e30b7655918a2f47
pkienzle <none@none>
2005-05-24 21:42:16 +0000

    Add GPL header

--- feedback.m
+++ feedback.m
@@ -1,3 +1,15 @@
+## Copyright (C) 2000 Ben Sapp.  All rights reserved.
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 2, or (at your option) any
+## later version.
+##
+## This is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+## for more details.
+
 ## feedback(sys1,sys2)
 ##
 ## Filter the output of sys1 through sys2 and subtract it from the input.
