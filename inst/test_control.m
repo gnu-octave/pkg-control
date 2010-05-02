b7bc126e90aaed96f16bcd754c7ef32eedafe06c
paramaniac <none@none>
2010-05-02 12:33:24 +0000

    control: add test_control.m (execute all available tests at once)

---
+++ inst/test_control.m
@@ -0,0 +1,18 @@
+## execute all available tests at once
+
+test ltimodels
+test hinfsyn
+test h2syn
+test place
+test care
+
+test kalman
+test lyap
+test dlyap
+test gram
+test covar
+
+test margin
+test sigma
+test initial
+test hsvd
