53c40e981936a2faa7790b2a1142bc980e242242
paramaniac <none@none>
2010-09-15 19:55:22 +0000

    control: add draft code

---
+++ inst/@lti/dssdata.m
@@ -0,0 +1,45 @@
+## Copyright (C) 2010   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{a}, @var{b}, @var{c}, @var{d}, @var{e}, @var{tsam}] =} dssdata (@var{sys})
+## Access descriptor state-space model data.
+## @end deftypefn
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: September 2010
+## Version: 0.1
+
+function [a, b, c, d, e, tsam] = dssdata (sys, flg = 0)
+
+  ## NOTE: this function is just a stub for development use
+
+  if (! isa (sys, "ss"))
+    sys = ss (sys);
+  endif
+
+  [a, b, c, d] = __sys_data__ (sys);
+
+  if (isempty (flg))     # dssdata (sys, [])
+    e = [];              # don't return eye for ss models
+  else                   # dssdata (sys)
+    e = eye (size (a));  # descriptor model
+  endif
+
+  tsam = sys.tsam;
+
+endfunction
