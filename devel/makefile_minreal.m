ec21e48ad6ac4806d6fc5e9a97a754fc55197c61
paramaniac <none@none>
2010-10-05 07:44:52 +0000

    control: add compatibility notes

--- devel/makefile_minreal.m
+++ devel/makefile_minreal.m
@@ -4,6 +4,9 @@
 ## USAGE: * fetch control from Octave-Forge by svn
 ##        * add control/inst, control/src and control/devel to your Octave path
 ##        * run makefile_*
+## NOTES: * The option "-Wl,-framework" "-Wl,vecLib" is needed for MacPorts'
+##          octave-devel @3.3.52_1+gcc44 on MacOS X 10.6.4. However, this option
+##          breaks other platforms. See MacPorts Ticket #26640.
 ## ==============================================================================
 
 homedir = pwd ();
