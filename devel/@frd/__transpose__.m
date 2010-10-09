1d9bd8bd0b3fa17b06331f085e3ee28606c9ca13
paramaniac <none@none>
2010-10-09 08:15:33 +0000

    control: add transpose

---
+++ devel/@frd/__transpose__.m
@@ -0,0 +1,35 @@
+## Copyright (C) 2010   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## Transpose of FRD models.
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: October 2010
+## Version: 0.1
+
+function sys = __transpose__ (sys)
+
+  H = sys.H;
+
+  for k = 1 : size (H, 3)
+    H(:, :, k) = H(:, :, k).'
+  endfor
+
+  sys.H = H;
+
+endfunction
