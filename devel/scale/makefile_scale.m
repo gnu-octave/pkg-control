940044e8df3ab404f92f3cc26ef0225b78a8c667
paramaniac <none@none>
2011-06-25 15:45:03 +0000

    control: add some files for dss scaling

--- devel/scale/makefile_scale.m
+++ devel/scale/makefile_scale.m
@@ -3,3 +3,7 @@ mkoctfile "-Wl,-framework" "-Wl,vecLib" \
           sltb01id.cc \
           TB01ID.f
 
+## scaling of descriptor state-space models
+## mkoctfile "-Wl,-framework" "-Wl,vecLib" \
+##           sltg01ad.cc \
+##           TG01AD.f
