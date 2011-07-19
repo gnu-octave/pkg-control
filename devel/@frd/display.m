fdb9fb25498c33f6217f6cb2d581189b8e3419a5
paramaniac <none@none>
2011-07-19 07:28:25 +0000

    control: draft code for frd models display routine

--- devel/@frd/display.m
+++ devel/@frd/display.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2010   Lukas F. Reichlin
+## Copyright (C) 2010, 2011   Lukas F. Reichlin
 ##
 ## This file is part of LTI Syncope.
 ##
@@ -24,6 +24,7 @@
 
 function display (sys)
 
+  sysname = inputname (1);
   [inname, outname, tsam] = __lti_data__ (sys.lti);
 
   [inname, m] = __labels__ (inname, "u");
@@ -31,19 +32,76 @@ function display (sys)
 
   disp ("");
 
-  disp (["Frequency response """, inputname(1), """ from input "]);
-
-  sys.H
-  sys.w
+  for i = 1 : m
+    disp (["Frequency response \"", sysname, "\" from input \"", inname{i}, "\" to output ..."]);
+    __disp_resp__ (sys.H(:,i,:), sys.w, outname);
+  endfor
 
   display (sys.lti);  # display sampling time
 
   if (tsam == -2)
     disp ("Static gain.");
   elseif (tsam == 0)
-    disp ("Continuous-time model.");
+    disp ("Continuous-time frequency response.");
   else
-    disp ("Discrete-time model.");
+    disp ("Discrete-time frequency response.");
+  endif
+
+endfunction
+
+
+function __disp_freq__ ()
+
+endfunction
+
+
+function __disp_resp__ (H, w, outname)
+
+  len = rows (w);
+  
+  % Hier kommt H, wie bei ss nur soviele Ausgänge pro Zeile anzeigen wie terminal size
+  % H kann auch komplex sein --> zwei Kolonnen
+
+  H = mat2cell (H, ones (1, rows (H)), 1, len)(:);
+  H = cellfun (@__resp2str__, H, outname, "uniformoutput", false)
+
+endfunction
+
+
+% Noch nicht in Betrieb, kann in display selbst aufgerufen werden, dann nur einmal aufrufen
+% und Resultat immer wieder übergeben
+function str = __freq2str__ (w, title = "w [rad/s]")
+
+  len = rows (w);
+  str = __vec2str__ (w);
+  line = repmat ("-", 1, max (columns (str), columns (title)));
+  str = strvcat (title, line, str);
+  space = repmat ("  ", len+2, 1);
+  str = [space, str];
+  
+endfunction
+
+
+% Für Cellfun
+function str = __resp2str__ (H, outname)
+
+  len = length (H);
+  str = __vec2str__ (H);
+  line = repmat ("-", 1, max (columns (str), columns (outname)));
+  str = strvcat (outname, line, str);
+  space = repmat ("  ", len+2, 1);
+  str = [space, str];
+  % Noch für complex ergänzen
+endfunction
+
+
+function col = __vec2str__ (vec, str)
+
+  vec = num2cell (vec(:));
+  col = cellfun (@(x) sprintf ("%.4f", x), vec, "uniformoutput", false);
+  col = strjust (char (col), "right");
+  if (nargin > 1)
+    col = [col, repmat(str, length (vec), 1)];
   endif
 
 endfunction
---
+++ devel/frd_display.m
@@ -0,0 +1,10 @@
+page_screen_output (0)
+
+%sys = Boeing707;
+%sys = frd (sys)
+
+%sys6 = frd ([1, 2; 3, 4], logspace (-1, 1, 10))
+
+sys = Boeing707;
+w = logspace (-1, 1, 10);
+sys = frd (sys, w)
