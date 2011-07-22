141c2cdedefc41804a8654b7bd705e611378590c
paramaniac <none@none>
2011-07-22 21:43:28 +0000

    control: fix frdata, improve texinfo string

--- inst/@lti/frdata.m
+++ inst/@lti/frdata.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2010   Lukas F. Reichlin
+## Copyright (C) 2010, 2011   Lukas F. Reichlin
 ##
 ## This file is part of LTI Syncope.
 ##
@@ -19,13 +19,36 @@
 ## @deftypefn {Function File} {[@var{H}, @var{w}, @var{tsam}] =} frdata (@var{sys})
 ## @deftypefnx {Function File} {[@var{H}, @var{w}, @var{tsam}] =} frdata (@var{sys}, @var{"vector"})
 ## Access frequency response data.
+## Argument @var{sys} is not limited to frequency response data objects.
+## If @var{sys} is not a frd object, it is converted automatically.
+##
+## @strong{Inputs}
+## @table @var
+## @item sys
+## Any type of LTI model.
+## @item "v", "vector"
+## In case @var{sys} is a SISO model, this option returns the frequency response
+## as a column vector (lw-by-1) instead of an array (p-by-m-by-lw).
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item H
+## Frequency response array (p-by-m-by-lw).  In the SISO case,
+## a vector (lw-by-1) is possible as well.
+## @item w
+## Frequency vector (lw-by-1) in radian per second [rad/s].
+## @item tsam
+## Sampling time in seconds.  If @var{sys} is a continuous-time model,
+## a zero is returned.
+## @end table
 ## @end deftypefn
 
 ## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
 ## Created: October 2010
-## Version: 0.1
+## Version: 0.2
 
-function [H, w, tsam] = frdata (sys, rtype = "vector")
+function [H, w, tsam] = frdata (sys, rtype = "array")
 
   if (! isa (sys, "frd"))
     sys = frd (sys);
@@ -35,7 +58,7 @@ function [H, w, tsam] = frdata (sys, rtype = "vector")
 
   tsam = sys.tsam;
 
-  if (rtype(1) == "v")
+  if (rtype(1) == "v" && issiso (sys))
     H = reshape (H, [], 1);
   endif
 
