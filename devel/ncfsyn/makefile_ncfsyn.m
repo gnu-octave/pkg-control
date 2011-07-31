9563f54a773b1b0b8a24f6f027922c573b07dadb
paramaniac <none@none>
2011-07-31 21:57:09 +0000

    control: add discrete-time support to ncfsyn (case D==0)

--- devel/ncfsyn/makefile_ncfsyn.m
+++ devel/ncfsyn/makefile_ncfsyn.m
@@ -6,3 +6,13 @@ mkoctfile "-Wl,-framework" "-Wl,vecLib" \
           MA02AD.f MB02PD.f MB01SD.f MB01UD.f SB03SY.f \
           MB01RX.f SB03MX.f SB03SX.f MB01RY.f SB03QY.f \
           SB03QX.f SB03MY.f SB04PX.f SB03MV.f SB03MW.f
+
+mkoctfile "-Wl,-framework" "-Wl,vecLib" \
+          slsb10kd.cc \
+          SB10KD.f SB02OD.f select.f SB02OY.f SB02OW.f \
+          SB02OV.f SB02MV.f SB02OU.f SB02MR.f
+
+mkoctfile "-Wl,-framework" "-Wl,vecLib" \
+          SB10ZD.f MA02AD.f SB02OD.f select.f MB01RX.f \
+          MB02VD.f SB02OY.f SB02OW.f SB02OV.f SB02OU.f \
+          SB02MR.f MA02GD.f SB02MV.f
---
+++ devel/ncfsyn/slsb10kd.cc
@@ -0,0 +1,148 @@
+/*
+
+Copyright (C) 2011   Lukas F. Reichlin
+
+This file is part of LTI Syncope.
+
+LTI Syncope is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LTI Syncope is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+Positive feedback controller for a discrete-time system (D == 0).
+Uses SLICOT SB10KD by courtesy of NICONET e.V.
+<http://www.slicot.org>
+
+Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+Created: July 2011
+Version: 0.1
+
+*/
+
+#include <octave/oct.h>
+#include <f77-fcn.h>
+#include "common.cc"
+
+extern "C"
+{ 
+    int F77_FUNC (sb10kd, SB10KD)
+                 (int& N, int& M, int& NP,
+                  double* A, int& LDA,
+                  double* B, int& LDB,
+                  double* C, int& LDC,
+                  double& FACTOR,
+                  double* AK, int& LDAK,
+                  double* BK, int& LDBK,
+                  double* CK, int& LDCK,
+                  double* DK, int& LDDK,
+                  double* RCOND,
+                  int* IWORK,
+                  double* DWORK, int& LDWORK,
+                  bool* BWORK,
+                  int& INFO);
+}
+     
+DEFUN_DLD (slsb10kd, args, nargout,
+   "-*- texinfo -*-\n\
+Slicot SB10KD Release 5.0\n\
+No argument checking.\n\
+For internal use only.")
+{
+    int nargin = args.length ();
+    octave_value_list retval;
+    
+    if (nargin != 4)
+    {
+        print_usage ();
+    }
+    else
+    {
+        // arguments in
+        Matrix a = args(0).matrix_value ();
+        Matrix b = args(1).matrix_value ();
+        Matrix c = args(2).matrix_value ();
+        
+        double factor = args(3).double_value ();
+        
+        int n = a.rows ();      // n: number of states
+        int m = b.columns ();   // m: number of inputs
+        int np = c.rows ();     // np: number of outputs
+        
+        int lda = max (1, n);
+        int ldb = max (1, n);
+        int ldc = max (1, np);
+        
+        int ldak = max (1, n);
+        int ldbk = max (1, n);
+        int ldck = max (1, m);
+        int lddk = max (1, m);
+        
+        // arguments out
+        int nk;
+        Matrix ak (ldak, n);
+        Matrix bk (ldbk, np);
+        Matrix ck (ldck, n);
+        Matrix dk (lddk, np);
+        ColumnVector rcond (4);
+        
+        // workspace
+        int liwork = 2 * max (n, np+m);
+        int ldwork = 15*n*n + 6*n +
+                     max (14*n+23, 16*n, 2*n+np+m, 3*(np+m)) +
+                     max (n*n, 11*n*np + 2*m*m + 8*np*np + 8*m*n + 4*m*np + np);
+
+        OCTAVE_LOCAL_BUFFER (int, iwork, liwork);
+        OCTAVE_LOCAL_BUFFER (double, dwork, ldwork);
+        OCTAVE_LOCAL_BUFFER (bool, bwork, 2*n);
+        
+        // error indicator
+        int info;
+
+
+        // SLICOT routine SB10KD
+        F77_XFCN (sb10kd, SB10KD,
+                 (n, m, np,
+                  a.fortran_vec (), lda,
+                  b.fortran_vec (), ldb,
+                  c.fortran_vec (), ldc,
+                  factor,
+                  ak.fortran_vec (), ldak,
+                  bk.fortran_vec (), ldbk,
+                  ck.fortran_vec (), ldck,
+                  dk.fortran_vec (), lddk,
+                  rcond.fortran_vec (),
+                  iwork,
+                  dwork, ldwork,
+                  bwork,
+                  info));
+
+        if (f77_exception_encountered)
+            error ("hinfsyn: slsb10kd: exception in SLICOT subroutine SB10KD");
+
+        if (info != 0)
+            error ("hinfsyn: slsb10kd: SB10KD returned info = %d", info);
+
+        // resizing
+        ak.resize (n, n);
+        bk.resize (n, np);
+        ck.resize (m, n);
+        dk.resize (m, np);
+        
+        // return values
+        retval(0) = ak;
+        retval(1) = bk;
+        retval(2) = ck;
+        retval(3) = dk;
+        retval(4) = rcond;
+    }
+    
+    return retval;
+}
