ac4fa4f9bc5abe8769f1b9106f62ba84e3696e56
paramaniac <none@none>
2011-08-15 16:44:45 +0000

    control: ss2tf draft code

--- devel/ss2tf/test_ss2tf.m
+++ devel/ss2tf/test_ss2tf.m
@@ -11,8 +11,48 @@ c = [  0.0   1.0   1.0
 d = [  1.0   0.0
        0.0   1.0 ];
 
-[gn, gd] = sltb04bd (a, b, c, d)
+[p, m] = size (d);
+md = 4
 
+[gn, gd, ign, igd] = sltb04bd (a, b, c, d)
+
+num = reshape (gn, md, p, m)
+den = reshape (gd, md, p, m)
+
+numc = mat2cell (num, md, ones(1,p), ones(1,m))
+denc = mat2cell (den, md, ones(1,p), ones(1,m))
+
+numc = squeeze (numc)
+denc = squeeze (denc)
+
+ignc = mat2cell (ign, ones(1,p), ones(1,m));
+igdc = mat2cell (igd, ones(1,p), ones(1,m));
+
+num = cellfun (@(x, y) x(1:y+1), numc, ignc, "uniformoutput", false);
+den = cellfun (@(x, y) x(1:y+1), denc, igdc, "uniformoutput", false);
+
+
+tf (num, den)
+
+%{
+numc{1,2,1}
+denc{1,2,1}
+numc{1,1,2}
+denc{1,1,2}
+%}
+
+
+%num(:, 1, 1)
+%den(:, 1, 1)
+%num(:, 1, 2)
+%den(:, 1, 2)
+
+%num = mat2cell (gn, p, m)
+%den = mat2cell (gd, p, m)
+
+%num = gn(1:ign+1)
+%den = gd(1:igd+1)
+%{
 [gn, gd, ign, igd] = sltb04bd (-2, 3, 4, 5)
 
 
@@ -40,3 +80,4 @@ P
 sys = WestlandLynx;
 
 [num, den, ign, igd] = sltb04bd (sys.a, sys.b, sys.c, sys.d);
+%}
