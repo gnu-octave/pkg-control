4edd3acdd6e41c0b6b96c4e39a0f7a6ef33404a4
paramaniac <none@none>
2011-08-19 06:44:15 +0000

    control: restore old conversion routine as a backup method for non-proper models

--- inst/@ss/__sys2tf__.m
+++ inst/@ss/__sys2tf__.m
@@ -24,27 +24,49 @@
 
 function [retsys, retlti] = __sys2tf__ (sys)
 
-  [a, b, c, d, tsam, scaled] = ssdata (sys);  # system could be a descriptor model
+  try
+    [a, b, c, d, tsam, scaled] = ssdata (sys);  # system could be a descriptor model
 
-  [num, den, ign, igd, md, p, m] = sltb04bd (a, b, c, d, scaled);
+    [num, den, ign, igd, md, p, m] = sltb04bd (a, b, c, d, scaled);
 
-  num = reshape (num, md, p, m);
-  den = reshape (den, md, p, m);
+    num = reshape (num, md, p, m);
+    den = reshape (den, md, p, m);
 
-  num = mat2cell (num, md, ones(1,p), ones(1,m));
-  den = mat2cell (den, md, ones(1,p), ones(1,m));
+    num = mat2cell (num, md, ones(1,p), ones(1,m));
+    den = mat2cell (den, md, ones(1,p), ones(1,m));
 
-  num = squeeze (num);
-  den = squeeze (den);
+    num = squeeze (num);
+    den = squeeze (den);
 
-  ign = mat2cell (ign, ones(1,p), ones(1,m));
-  igd = mat2cell (igd, ones(1,p), ones(1,m));
+    ign = mat2cell (ign, ones(1,p), ones(1,m));
+    igd = mat2cell (igd, ones(1,p), ones(1,m));
 
-  num = cellfun (@(x, y) x(1:y+1), num, ign, "uniformoutput", false);
-  den = cellfun (@(x, y) x(1:y+1), den, igd, "uniformoutput", false);
+    num = cellfun (@(x, y) x(1:y+1), num, ign, "uniformoutput", false);
+    den = cellfun (@(x, y) x(1:y+1), den, igd, "uniformoutput", false);
+  catch
+    ## sys.e was probably singular, therefore ssdata failed.
+    ## The method below works for non-proper models, but it
+    ## requires the model to be SISO.
+    warning ("ss: ss2tf: TB04BD failed, trying backup method now");
 
-  retsys = tf (num, den, tsam);               # tsam needed to set appropriate tfvar
-  retlti = sys.lti;                           # preserve lti properties
+    if (! issiso (sys))
+      error ("ss: ss2tf: backup method works for SISO models only");
+    endif
+
+    if (isempty (sys.a))                        # static gain
+      num = sys.d;
+      den = 1;
+    else                                        # default case
+      [zer, gain] = zero (sys);
+      pol = pole (sys);
+      num = gain * real (poly (zer));
+      den = real (poly (pol));
+    endif
+    tsam = get (sys, "tsam");
+  end_try_catch
+
+  retsys = tf (num, den, tsam);                 # tsam needed to set appropriate tfvar
+  retlti = sys.lti;                             # preserve lti properties
   
   ## FIXME: sys = tf (ss (5))
 
