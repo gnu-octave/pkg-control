117d79551593b11fe1d7378a83e267d6f315e70d
paramaniac <none@none>
2011-09-24 03:39:13 +0000

    control: use chapters instead of sections for each command group

--- devel/pdfdoc/function_doc.m
+++ devel/pdfdoc/function_doc.m
@@ -19,15 +19,18 @@ for k = 1:numel (list {1}.provides)
         group = list {1}.provides{k};
         functions = group.functions;
 
-        fprintf (fid, '@section %s\n', group.category);
+        % fprintf (fid, '@section %s\n', group.category);
+        fprintf (fid, '@chapter %s\n', group.category);
         
         for k=1:numel(functions)
                 [TEXT, FORMAT] = get_help_text (functions(k));
                 fun = functions{k};
                 if (fun(1) == "@")
-                  fprintf (fid, '@subsection @%s\n', fun);
+                  % fprintf (fid, '@subsection @%s\n', fun);
+                  fprintf (fid, '@section @%s\n', fun);
                 else
-                  fprintf (fid, '@subsection %s\n', fun);
+                  % fprintf (fid, '@subsection %s\n', fun);
+                  fprintf (fid, '@section %s\n', fun);
                 endif
                 fprintf (fid,TEXT);
         end
