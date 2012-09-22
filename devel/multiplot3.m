67fbd8ce61af4bae135a0ed1ecf03d877c20753b
paramaniac <none@none>
2012-09-22 10:25:04 +0000

    control: remove cruft

--- devel/multiplot3.m
+++ devel/multiplot3.m
@@ -1,10 +1,10 @@
 load tfs.dat
 
 figure (1)
-step2 (T_AH, T_opt)
+step (T_AH, T_opt)
 
 figure (2)
-step2 (T_AH, '-.r', T_opt, '-b')
+step (T_AH, '-.r', T_opt, '-b')
 
 figure (3)
-step2 (T_AH, c2d (T_opt, 1))
+step (T_AH, c2d (T_opt, 1))
