e5475a1c50ad4eb6be7b47d5cea30d92eb33521b
paramaniac <none@none>
2012-10-29 06:57:23 +0000

    control: update news file

--- devel/matched_d2c.m
+++ devel/matched_d2c.m
@@ -2,4 +2,10 @@ numP = [1];
 denP = conv ([1, 1, 1], [1, 4, 6, 4, 1]);
 P = tf (numP, denP);
 
+
+Pd = c2d (P, 0.1, 'matched')
+[z, k] = zero (Pd)
+p = pole (Pd)
+
+
 Pc = d2c (c2d (P, 0.1, 'matched'), 'matched')
