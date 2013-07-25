28cafeacb98ec0d5acee4a79cf1c93e1b1daa3f6
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2013-07-25 21:32:28 +0200

    touch up szero

--- devel/szero.m
+++ devel/szero.m
@@ -22,78 +22,76 @@
 
 function [z, Rank] = szero (sys)
 
+  if (nargin != 1)
+    print_usage ();
+  endif
+
   [a, b, c, d] = ssdata (sys);
+  [pp, mm] = size (sys);
+  nn = rows (a);
 
-  nn = size(a,1);
-  pp = size(c,1);
-  mm = size(b,2);
+  ## Tolerance for intersection of zeros
+  Zeps = 10 * sqrt ((nn+pp)*(nn+mm)) * eps * norm (a,'fro');
 
-  % Tolerance for intersection of zeros
-  Zeps = 10*sqrt((nn+pp)*(nn+mm))*eps*norm(a,'fro');
+  [z, ~, Rank] = zero (sys);
 
-  [z, ~, Rank] = zero (ss (a,b,c,d));
-  z = sort (z(:));
-
-  if Rank == 0
+  if (Rank == 0 || (Rank == min(pp,mm) && mm == pp))  ## sys is not degenerated and square
+    z = sort (z(:));
     return;
   endif
 
-  if Rank==min(pp,mm) & mm==pp
-  % System (A,B,C,D) is not degenerated and square
-    return
-  else % System (A,B,C,D) is degenerated and/or non-square
-    z = [];
-    %
-    % Computation of the greatest common divisor of all minors of the 
-    % Rosenbrock system matrix that have the following form
-    %
-    %    1, 2, ..., n, n+i_1, n+i_2, ..., n+i_k
-    %   P
-    %    1, 2, ..., n, n+j_1, n+j_2, ..., n+j_k
-    % 
-    % with k = Rank.
-    %
-    NKP = nchoosek(1:pp,Rank);
-    [IP,JP] = size(NKP);
-    NKM = nchoosek(1:mm,Rank);
-    [IM,JM] = size(NKM);
-    for i = 1:IP
-      for j = 1:JP
-        k = NKP(i,j);
-        C1(j,:) = c(k,:); % Build C of dimension (Rank x n)
+  ## System (A,B,C,D) is degenerated and/or non-square
+  z = [];
+  ##
+  ## Computation of the greatest common divisor of all minors of the 
+  ## Rosenbrock system matrix that have the following form
+  ##
+  ##    1, 2, ..., n, n+i_1, n+i_2, ..., n+i_k
+  ##   P
+  ##    1, 2, ..., n, n+j_1, n+j_2, ..., n+j_k
+  ## 
+  ## with k = Rank.
+  ##
+  NKP = nchoosek (1:pp, Rank);
+  [IP, JP] = size (NKP);
+  NKM = nchoosek (1:mm, Rank);
+  [IM, JM] = size (NKM);
+  for i = 1:IP
+    for j = 1:JP
+      k = NKP(i,j);
+      C1(j,:) = c(k,:);         # Build C of dimension (Rank x n)
+    endfor
+    for ii = 1:IM
+      for jj = 1:JM
+        k = NKM(ii,jj);
+        B1(:,jj) = b(:,k);      # Build B of dimension (n x Rank)
       endfor
-      for ii = 1:IM
-        for jj = 1:JM
-          k = NKM(ii,jj);
-          B1(:,jj) = b(:,k); % Build B of dimension (n x Rank)
-        endfor
-        [z1,~,rank1] = zero (ss (a, B1, C1, zeros (Rank, Rank)));
-        if rank1 == Rank
-          if isempty(z1)
-            z = sort (z1(:));    % Subsystem has no zeros -> system has no system zeros
-            return;
-          else
-            if isempty(z)
-              z = z1;  % Zeros of the first subsystem
-            else    % Compute intersection of z and z1 with tolerance Zeps 
-              z2=[];
-              for ii=1:length(z)
-                for jj=1:length(z1)
-                  if abs(z(ii)-z1(jj)) < Zeps
-                    z2(length(z2)+1)=z(ii);
-                    z1(jj)=[];
-                    break
-                  endif
-                endfor
+      [z1, ~, rank1] = zero (ss (a, B1, C1, zeros (Rank, Rank)));
+      if (rank1 == Rank)
+        if (isempty (z1))
+          z = sort (z1(:));     # Subsystem has no zeros -> system has no system zeros
+          return;
+        else
+          if (isempty (z))
+            z = z1;             # Zeros of the first subsystem
+          else                  # Compute intersection of z and z1 with tolerance Zeps 
+            z2 = [];
+            for ii=1:length(z)
+              for jj=1:length(z1)
+                if (abs (z(ii)-z1(jj)) < Zeps)
+                  z2(length(z2)+1) = z(ii);
+                  z1(jj) = [];
+                  break;
+                endif
               endfor
-              z = z2; % System zeros are the common zeros of all subsystems
-            endif
+            endfor
+            z = z2;             # System zeros are the common zeros of all subsystems
           endif
         endif
-      endfor
+      endif
     endfor
-  endif
-  
+  endfor
+
   z = sort (z(:));
 
 endfunction
