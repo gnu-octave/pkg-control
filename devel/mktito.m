09021fbc070c87d96b35df4755f5c3c33415b99b
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2013-10-04 12:28:43 +0200

    group: mktito: reuse doc from h2syn
    --HG--
    branch : group

--- devel/mktito.m
+++ devel/mktito.m
@@ -1,23 +1,97 @@
-function sys = mktito (sys, nmeas, ncont)
+## Copyright (C) 2013   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn{Function File} {@var{P} =} h2syn (@var{P}, @var{nmeas}, @var{ncon})
+## Partition @acronym{LTI} plant @var{P} for robust controller synthesis.
+##
+## @strong{Inputs}
+## @table @var
+## @item P
+## Generalized plant.
+## @item nmeas
+## Number of measured outputs v.  The last @var{nmeas} outputs of @var{P} are connected to the
+## inputs of controller @var{K}.  The remaining outputs z (indices 1 to p-nmeas) are used
+## to calculate the H-2/H-infinity norm.
+## @item ncon
+## Number of controlled inputs u.  The last @var{ncon} inputs of @var{P} are connected to the
+## outputs of controller @var{K}.  The remaining inputs w (indices 1 to m-ncon) are excited
+## by a harmonic test signal.
+## @end table
+##
+## @strong{Outputs}
+## @table @var
+## @item P
+## Partitioned plant.
+## @end table
+##
+## @strong{Block Diagram}
+## @example
+## @group
+##
+## min||N(K)||             N = lft (P, K)
+##  K         norm
+##
+##                +--------+  
+##        w ----->|        |-----> z
+##                |  P(s)  |
+##        u +---->|        |-----+ v
+##          |     +--------+     |
+##          |                    |
+##          |     +--------+     |
+##          +-----|  K(s)  |<----+
+##                +--------+
+##
+##                +--------+      
+##        w ----->|  N(s)  |-----> z
+##                +--------+
+## @end group
+## @end example
+##
+## @end deftypefn
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: October 2013
+## Version: 0.1
+
+function P = mktito (P, nmeas, ncon)
 
   if (nargin != 3)
     print_usage ();
   endif
 
-  if (! isa (sys, "lti"))
+  if (! isa (P, "lti"))
     error ("mktito: first argument must be an LTI model");
   endif
   
-  [p, m] = size (sys);
+  [p, m] = size (P);
   
   ## TODO: improve argument checking
-  if (! is_real_scalar (nmeas, ncont))
-    print_usage ();
+  if (! is_real_scalar (nmeas))
+    error ("mktito: second argument invalid");
+  endif
+  
+  if (! is_real_scalar (ncon))
+    error ("mktito: third argument invalid");
   endif
   
   outgroup = struct ("Y1", 1:p-nmeas, "Y2", p-nmeas+1:p);
-  ingroup = struct ("U1", 1:m-ncont, "U2", m-ncont+1:m);
+  ingroup = struct ("U1", 1:m-ncon, "U2", m-ncon+1:m);
 
-  sys = set (sys, "outgroup", outgroup, "ingroup", ingroup);
+  P = set (P, "outgroup", outgroup, "ingroup", ingroup);
 
 endfunction
