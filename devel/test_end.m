38072610bfad75658585446c377058044e6d2394
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2013-12-03 09:44:39 +0100

    end: end indexing for LTI models

---
+++ devel/test_end.m
@@ -0,0 +1,6 @@
+P = BMWengine
+P(1:end, 1:end)
+P(end:-1:1, end:-1:1)
+
+G = mktito (P, 1, 2)
+G(end:-1:1, end:-1:1)
---
+++ inst/@lti/end.m
@@ -0,0 +1,43 @@
+## Copyright (C) 2009-2013   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## End indexing for @acronym{LTI} objects.
+## Used by Octave for "sys(1:end, end-1)".
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: December 2013
+## Version: 0.1
+
+function ret = end (sys, k, n)
+
+  if (n != 2)
+    error ("lti: end: require 2 indices in the expression");
+  endif
+
+  [p, m] = size (sys);
+
+  switch (k)
+    case 1
+      ret = p;
+    case 2
+      ret = m;
+    otherwise
+      error ("lti: end: invalid expression index k = %d", k);
+  endswitch
+
+endfunction
