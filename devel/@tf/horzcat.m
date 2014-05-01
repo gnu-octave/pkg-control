86d1886ec76b7ddcbed1e4b10f8bc1a4339ca6fc
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2014-05-01 11:48:25 +0200

    tf: horzcat, vertcat: add docstring

--- devel/@tf/horzcat.m
+++ devel/@tf/horzcat.m
@@ -1,3 +1,30 @@
+## Copyright (C) 2009-2014   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## Horizontal concatenation of @acronym{TF} objects.
+## Used by Octave for "[sys1, sys2]".
+## Avoids conversion to state-space and back by overriding
+## the general horzcat function for @acronym{LTI} objects.
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: April 2014
+## Version: 0.1
+
 function sys = horzcat (sys, varargin)
 
   warning ("tf: horzcat!");
--- devel/@tf/vertcat.m
+++ devel/@tf/vertcat.m
@@ -1,3 +1,30 @@
+## Copyright (C) 2009-2014   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## Vertical concatenation of @acronym{TF} objects.
+## Used by Octave for "[sys1; sys2]".
+## Avoids conversion to state-space and back by overriding
+## the general vertcat function for @acronym{LTI} objects.
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: May 2014
+## Version: 0.1
+
 function sys = vertcat (sys, varargin)
 
   warning ("tf: vertcat!");
