b9470bc277a8f5e22149e4dc1b2ef5c1fa8dfa27
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2014-05-06 09:01:18 +0200

    lti: times: remove cruft

--- inst/@lti/times.m
+++ inst/@lti/times.m
@@ -46,10 +46,8 @@ function sys = times (sys1, sys2)
   if (p1 != p2 || m1 != m2)
     if (p1 == 1 && m1 == 1 && p2*m2 > 1)        # sys1 SISO, sys2 non-empty
       sys1 = repmat (sys1, p2, m2);
-      [p1, m1] = size (sys1);
     elseif (p2 == 1 && m2 == 1 && p1*m1 > 1)    # sys2 SISO, sys1 non-empty
       sys2 = repmat (sys2, p1, m1);
-      [p2, m2] = size (sys2);
     else
       error ("lti: times: system dimensions incompatible: (%dx%d) .* (%dx%d)", ...
               p1, m1, p2, m2);
