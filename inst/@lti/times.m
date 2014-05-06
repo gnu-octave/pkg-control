3dc8cefd6f7337dfa7659d9c5783b8e7cc94c45d
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2014-05-06 08:48:28 +0200

    lti: times, repmat: move files into place, avoid code duplication

--- inst/@lti/times.m
+++ inst/@lti/times.m
@@ -45,9 +45,11 @@ function sys = times (sys1, sys2)
   
   if (p1 != p2 || m1 != m2)
     if (p1 == 1 && m1 == 1 && p2*m2 > 1)        # sys1 SISO, sys2 non-empty
-      [sys1, p1, m1] = __siso_expansion__ (sys1, p2, m2);
+      sys1 = repmat (sys1, p2, m2);
+      [p1, m1] = size (sys1);
     elseif (p2 == 1 && m2 == 1 && p1*m1 > 1)    # sys2 SISO, sys1 non-empty
-      [sys2, p2, m2] = __siso_expansion__ (sys2, p1, m1);
+      sys2 = repmat (sys2, p1, m1);
+      [p2, m2] = size (sys2);
     else
       error ("lti: times: system dimensions incompatible: (%dx%d) .* (%dx%d)", ...
               p1, m1, p2, m2);
@@ -55,20 +57,5 @@ function sys = times (sys1, sys2)
   endif
   
   sys = __times__ (sys1, sys2);
-
-endfunction
-
-
-function [sys, p, m] = __siso_expansion__ (sys, rows, cols)
-
-  tmp = cell (cols, 1);
-  tmp(1:cols) = sys;
-  sys = horzcat (tmp{:});
-  
-  tmp = cell (rows, 1);
-  tmp(1:rows) = sys;
-  sys = vertcat (tmp{:});
-
-  [p, m] = size (sys);                          # SISO case: p = rows, m = cols
 
 endfunction
