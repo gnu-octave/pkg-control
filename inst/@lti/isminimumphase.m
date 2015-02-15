a48d00662a93d1789a2c8dfd6dcd2f0a9da0afb8
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-02-15 19:11:09 +0100

    isminimumphase: mention paper by Michael Zeitz in the documentation

--- inst/@lti/isminimumphase.m
+++ inst/@lti/isminimumphase.m
@@ -18,16 +18,29 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {@var{bool} =} isminimumphase (@var{sys})
 ## @deftypefnx {Function File} {@var{bool} =} isminimumphase (@var{sys}, @var{tol})
-## Determine whether @acronym{LTI} system is minimum-phase.
+## Determine whether @acronym{LTI} system has asymptotically stable zero dynamics.
 ## According to the definition of Byrnes/Isidori [1], the zeros
 ## of a minimum-phase system must be strictly
 ## inside the left complex half-plane (continuous-time case)
 ## or inside the unit circle (discrete-time case).
-## The poles are not tested.
-## Note that the definitions from Byrnes/Isidori [1] and Bode [2] are different.
-## The name minimum-phase refers to the fact that such a system has the
-## minimum possible phase lag for the given magnitude response |sys(jw)|.
+## Note that the poles are not tested.
 ##
+## M. Zeitz [2] discusses the inconsistent definitions of the minimum-phase property
+## in a German paper.  The abstract in English states the following [2]:
+## 
+## Originally, the minimum phase property has been defined by H. W. Bode [3]
+## in order to characterize the unique relationship between gain and phase of
+## the frequency response.
+## With regard to the design of digital filters, another definition of minimum
+## phase is used and a filter is said to be minimum phase if both the filter
+## and its inverse are asymptotically stable.
+## Finally, systems with asymptotically stable zero dynamics are named as
+## minimum phase by C. I. Byrnes and A. Isidori [1].
+## Due to the inconsistent definitions, avoiding the minimum phase property
+## for control purposes is advocated and the well-established criteria of
+## Hurwitz or Ljapunow to describe the stability of filters and zero dynamics
+## are recommended.
+## 
 ## @strong{Inputs}
 ## @table @var
 ## @item sys
@@ -56,9 +69,13 @@
 ## @cite{A Frequency Domain Philosophy for Nonlinear Systems}.
 ## IEEE Conf. Dec. Contr. 23, pp. 1569–1573, 1984.
 ##
-## [2] Bode, H.W.
+## [2] Zeitz, M.
+## @cite{Minimum phase – no relevant property of automatic control!}.
+## at – Automatisierungstechnik. Volume 62, Issue 1, pp. 3–10, 2014.
+##
+## [3] Bode, H.W.
 ## @cite{Network Analysis and Feedback Amplifier Design}.
-## D. Van Nostrand Company, pp. 312-318, 1945
+## D. Van Nostrand Company, pp. 312-318, 1945.
 ## pp. 341-351, 1992.
 ## @end deftypefn
 
