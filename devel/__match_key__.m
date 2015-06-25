b40f211f0c92eab5b25bc35006cc981476767b49
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-06-25 09:47:40 +0200

    add draft code for partial matching of property names

---
+++ devel/__match_key__.m
@@ -0,0 +1,31 @@
+function key = __match_key__ (str, props, caller = "match_key")
+
+  if (! ischar (str))
+    error ("%s: property name must be a string", caller);
+  endif
+
+  ## exact matching - needed for e.g. iddata properties "u" and "userdata"
+  idx = strcmpi (str, props);
+  n = sum (idx);
+  
+  if (n == 1)           # 1 exact match
+    key = lower (str);
+    return;
+  elseif (n > 1)        # props are not unique, this would be a bug in the control package
+    error ("%s: property name '%s' is ambiguous", caller, str);
+  endif
+  
+  ## partial matching - n was zero
+  idx = strncmpi (str, props, length (str))
+  n = sum (idx);
+  
+  if (n == 1)
+    key = lower (props{idx});
+    return;
+  else (n > 1)
+    error ("%s: property name '%s' is ambiguous", caller, str);
+  endif
+  
+  error ("%s: property name '%s' is unknown", caller, str);
+  
+endfunction
