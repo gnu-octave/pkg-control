dc66f84a74e34f886f3941558d703dabe0436fab
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-06-28 07:08:02 +0200

    match_key: add copyright header and texinfo docstring

--- inst/__match_key__.m
+++ inst/__match_key__.m
@@ -1,3 +1,32 @@
+## Copyright (C) 2009-2015   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## Check whether string @var{str} matches one of the candidates
+## in cell @var{props} exactly and return the matching property/key
+## name.  If no exact match is found, check whether there is a
+## candidate name in @var{props} which starts with @var{str} and
+## return the partial match.  In case of ambiguity or a mismatch,
+## raise an error.
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: June 2015
+## Version: 0.1
+
 function key = __match_key__ (str, props, caller = "match_key")
 
   if (! ischar (str))
