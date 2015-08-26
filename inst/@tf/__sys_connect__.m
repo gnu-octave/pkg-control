02ed70d827ac54dcf77935e98e47ba86171e37c8
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-08-26 08:36:52 +0200

    tf: fix bug reported by Piet Wertelaers by checking some denominators. MIMO tf interconnections were mistakenly handled by shortcuts for SISO tf models.

--- inst/@tf/__sys_connect__.m
+++ inst/@tf/__sys_connect__.m
@@ -35,7 +35,7 @@
 
 ## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
 ## Created: October 2009
-## Version: 0.2
+## Version: 0.3
 
 function sys = __sys_connect__ (sys, M)
 
@@ -43,18 +43,6 @@ function sys = __sys_connect__ (sys, M)
 
   num = sys.num;
   den = sys.den;
-
-  ## TODO: Implementation for MIMO models.  There are three possibilities:
-  ##       1. An _algebraic_ solution of the inversion problem in order
-  ##          to not introduce unwanted zero/pole pairs.  Difficult.
-  ##       2. A numeric solution of the inversion problem.  Afterwards,
-  ##          elimination of _all_ zero/pole pairs by minreal.  Bad.
-  ##       3. Conversion to state-space, solving the problem there and
-  ##          converting back to transfer function.  Easier, but obviously,
-  ##          this way needs MIMO __sys2ss__ and __sys2tf__ implementations
-  ##          as described in Thomas Kailath's classic "Linear Systems".
-  ##          Possibly this is the way to go, but it works for proper systems
-  ##          only unless descriptor state-space models are implemented.
 
   ## WARNING: The code below is a cheap hack to quickly enable SISO TF connections.
 
@@ -73,7 +61,7 @@ function sys = __sys_connect__ (sys, M)
   elseif (p == 3 && m == 4 && num{1,3} == 0 && num{1,4} == 0 ...
           && num{2,1} == 0 && num{2,2} == 0 && num{2,4} == 0 ...
           && num{3,1} == 0 && num{3,2} == 0 && num{3,3} == 0 ...
-          && M == [0, 1, 0; 0, 0, 1; 0, 0, 0; 0, 0, 0])
+          && isequal (M, [0, 1, 0; 0, 0, 1; 0, 0, 0; 0, 0, 0]))
     ## horzcat [sys1, sys2], plus, minus
     sys.num(:) = tfpoly (0);
     sys.den(:) = tfpoly (1);
@@ -91,18 +79,18 @@ function sys = __sys_connect__ (sys, M)
     sys.den(3,4) = den{3,4};
 
   elseif (p == 3 && m == 3 && num{1,3} == 0 ...
-          && num{2,1} == 0 && num{2,2} == 0 && num{2,3} == 1 ...
-          && num{3,1} == 0 && num{3,2} == 0 && num{3,3} == 1 ...
-          && M == [0, 1, 0; 0, 0, 1; 0, 0, 0])
+          && num{2,1} == 0 && num{2,2} == 0 && num{2,3} == 1 && den{2,3} == 1 ...
+          && num{3,1} == 0 && num{3,2} == 0 && num{3,3} == 1 && den{3,3} == 1 ...
+          && isequal (M, [0, 1, 0; 0, 0, 1; 0, 0, 0]))
     ## plus, minus
     sys.num(1,3) = num{1,1} * den{1,2}  +  num{1,2} * den{1,1};
     sys.den(1,3) = den{1,1} * den{1,2};
 
   elseif (p == 4 && m == 3 && num{1,2} == 0 && num{1,3} == 0 ...
           && num{2,1} == 0 && num{2,3} == 0 ...
-          && num{3,1} == 0 && num{3,2} == 0 && num{3,3} == 1 ...
-          && num{4,1} == 0 && num{4,2} == 0 && num{4,3} == 1 ...
-          && M == [0, 0, 1, 0; 0, 0, 0, 1; 0, 0, 0, 0])
+          && num{3,1} == 0 && num{3,2} == 0 && num{3,3} == 1 && den{3,3} == 1 ...
+          && num{4,1} == 0 && num{4,2} == 0 && num{4,3} == 1 && den{4,3} == 1 ...
+          && isequal (M, [0, 0, 1, 0; 0, 0, 0, 1; 0, 0, 0, 0]))
     ## vertcat [sys1; sys2]
     sys.num(1,3) = num{1,1};
     sys.num(2,3) = num{2,2};
