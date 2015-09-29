3a2f4d83b09681fff78a1404876ddcd412db98e4
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-09-29 21:01:18 +0200

    pid: add missing copyright header

--- devel/pid.m
+++ devel/pid.m
@@ -1,3 +1,20 @@
+## Copyright (C) 2009-2015   Lukas F. Reichlin
+##
+## This file is part of LTI Syncope.
+##
+## LTI Syncope is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## LTI Syncope is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+
 ## -*- texinfo -*-
 ## @deftypefn{Function File} {@var{C} =} pid (@var{Kp})
 ## @deftypefnx{Function File} {@var{C} =} pid (@var{Kp}, @var{Ki})
@@ -14,6 +31,14 @@
 ## @end group
 ## @end example
 ## @end deftypefn
+
+## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
+## Created: June 2015
+## Version: 0.1
+
+## TODO: discrete-time case, 'pidstd' function, dozens of options, ...
+##       If you wish to kill time with this repetitive task,
+##       I'm happy to add your work :-)
 
 function C = pid (Kp = 1, Ki = 0, Kd = 0, Tf = 0)
 
