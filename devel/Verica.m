66dfd1395f0d257be44064b747b313c377696875
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-10-12 17:20:06 +0200

    Verica: add comments

--- devel/Verica.m
+++ devel/Verica.m
@@ -1,3 +1,4 @@
+% Plant
 A = [ -0.01357      -32.2          -46.3            0
        0.00012        0              1.214          0
       -0.0001212      0             -1.214          1
@@ -23,6 +24,7 @@ sys = ss (A, B, C, D);
 sys = set (sys, 'inputname', 'u', 'outputname', 'y')
 
 
+% Controller
 lambda_sys_desired = [ -0.5   -1+1j   -1-1j   -2  ]
 
 F = place (sys, lambda_sys_desired)
@@ -31,6 +33,7 @@ ctrl = ss (-F);
 ctrl = set (ctrl, 'inputname', 'xhat', 'outputname', 'u')
 
 
+% Observer
 lambda_obs_desired = [ -10    -11     -12     -13 ]
 
 K = place (sys.', lambda_obs_desired).'
@@ -39,20 +42,25 @@ obs = ss (A-K*C, [B, K]);
 obs = set (obs, 'inputname', {'u', 'y1', 'y2'}, 'outputname', 'xhat')
 
 
+% Gain
 G = ss (C);
 G = set (G, 'inputname', 'xhat', 'outputname', 'yhat')
 
 
+% Output Error
 OE = sumblk ('e = y - yhat', 2)
 
 
+% Entire System
 N = connect (sys, ctrl, obs, G, OE, ':', {'e1', 'e2'})
 
 
+% Initial Conditions
 y0 = C * x0;
 x0hat = pinv (C.' * C) * C.' * y0;
 
 
+% Simulation
 figure (1)
 initial (N, [x0; x0hat], 1)
 
