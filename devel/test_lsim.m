fd09f4f530a27dfb5f2c05ab0eecad0391652778
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-10-21 10:20:04 +0200

    maint: update news

---
+++ devel/test_lsim.m
@@ -0,0 +1,25 @@
+## Examples from <http://ch.mathworks.com/help/control/ref/lsim.html>
+
+figure (1)
+H = [tf([2 5 1],[1 2 3]);tf([1 -1],[1 1 5])];
+[u,t] = gensig('square',4,10,0.1);
+lsim(H,u,t)
+
+
+figure (2)
+w2 = 62.83^2;
+h = tf(w2,[1 2 w2]);
+t = 0:0.1:5;                % vector of time samples
+u = (rem(t,1) >= 0.5);        % square wave values
+u = double (u);             # FIXME
+lsim(h,u,t)
+
+
+figure (3)
+dt = 0.016;
+ts = 0:dt:5;
+us = (rem(ts,1) >= 0.5);
+us = double (us);           # FIXME
+hd = c2d(h,dt);
+lsim(hd,us,ts)
+
