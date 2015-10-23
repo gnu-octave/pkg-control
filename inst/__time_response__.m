5f7bc19a26a25df5fde770aec4d97a4cb5c73c9b
Lukas F. Reichlin <lukas.reichlin@gmail.com>
2015-10-23 17:25:27 +0200

    timeresp: add fixme comment

--- inst/__time_response__.m
+++ inst/__time_response__.m
@@ -140,6 +140,10 @@ function [y, t, x] = __time_response__ (response, args, plotflag)
     n_sys = nnz (sys_idx);
     sty = arrayfun (@(x) args(tmp == x), 1:n_sys, "uniformoutput", false);
 
+    ## FIXME: raise warning for strings before the first LTI model instead
+    ##        of simply ignoring them, e.g.  step ('style', sys1, ...)
+    ##        maybe also check this if we don't display a plot
+
     ## default plotting styles if empty
     colororder = get (gca, "colororder");
     rc = rows (colororder);
