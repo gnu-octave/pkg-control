a7eef27b627f6950125fef1c8aa8913637899e6e
doug <doug.dastew@gmail.com>
2019-03-25 17:10:50 -0400

    @ss/ss2ss.m: remove a blank line so html works.

--- inst/@ss/ss2ss.m
+++ inst/@ss/ss2ss.m
@@ -16,7 +16,6 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {@var{SYS_TRANSFORMED} =} ss2ss (@var{SYS}, @var{T})
 ## @deftypefnx {Function File} {[@var{A_TRANSFORMED} @var{B_TRANSFORMED} @var{C_TRANSFORMED} @var{D_TRANSFORMED}] =} ss2ss (@var{A},  @var{B}, @var{C}, @var{D}, @var{T})
-## 
 ## Applies the similarity transformation T to a state-space model
 ##
 ## Given the state space model
