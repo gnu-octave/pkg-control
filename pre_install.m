1bb81554653127f28b18f4d0c34820beb8bf3cb7
Torsten Lilge <ttl-octave@mailbox.org>
2021-06-19 10:10:45 +0200

    preparation of relese 3.3.0

--- pre_install.m
+++
@@ -1,4 +0,0 @@
-function pre_install (data)
-  fprintf ('\nThis version 3.2.91 is a release candidate for the\n');
-  fprintf ('next official release 3.3.0 of the control package.\n\n');
-endfunction
