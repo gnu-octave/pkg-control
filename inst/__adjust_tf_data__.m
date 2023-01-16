138e72e6da98ab0618b0329c10917a8a1b4fa2bb
Torsten Lilge <ttl-octave@mailbox.org>
2023-01-16 06:54:27 +0100

    improve detection of static gain systems (bug #63661
    * __adjust_tf_data__: detect scalars hidden in vectors with leading zeros
    
    --HG--
    branch : stable

--- inst/__adjust_tf_data__.m
+++ inst/__adjust_tf_data__.m
@@ -44,8 +44,8 @@ function [num, den, tsam, tfvar] = __adjust_tf_data__ (num, den, tsam = -2)
   endif
 
   ## Now check for static gain if all tfs have size num and size den of one
-  num_scalar = cellfun (@(p) length (p) == 1, num);
-  den_scalar = cellfun (@(p) length (p) == 1, den);
+  num_scalar = cellfun (@(p) (find (p != 0, 1) == length (p)) || (length (find (p != 0, 1)) == 0), num);
+  den_scalar = cellfun (@(p) (find (p != 0, 1) == length (p)) || (length (find (p != 0, 1)) == 0), den);
   if (all (num_scalar) && all (den_scalar))
     ## All tf components are of the form b0/a0 (static gain)
     tsam = -2;
