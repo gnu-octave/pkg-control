dfae5d51a9fa634eff3cac8ff9c2c4f8ebedad43
Torsten Lilge <ttl-octave@mailbox.org>
2024-01-03 20:14:53 +0100

    final updates before release: license information, release date

--- post_install.m
+++ post_install.m
@@ -1,17 +1,8 @@
 function post_install (d)
 
-  fprintf ('\n');
   fprintf ('The control package was installed into the directory\n');
   fprintf ('%s.\n', d.dir);
-  fprintf ('\n');
-  fprintf ('This package is released under the GPLv3+, which can be\n');
-  fprintf ('found in \"packinfo/COPYING\". License and copyright information\n');
-  fprintf ('of the used SLICOT-Reference routines (BSD 3-Clause) are located\n');
-  fprintf ('in \"doc/SLICOT\".\n');
-  fprintf ('\n');
-  fprintf ('The online documentation of the control package at\n');
-  fprintf ('https://gnu-octave.github.io/pkg-control/ can be accessed\n');
-  fprintf ('from within Octave with the command doc_control ().\n');
-  fprintf ('\n');
+  fprintf ('License and copyright information can be found in ');
+  fprintf ('\"packinfo/COPYING\"\n');
 
 end
