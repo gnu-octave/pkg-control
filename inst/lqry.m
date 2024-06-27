5431d1d533955c4b84d916cef76655fd6f6c6069
fdi86 <diiorio.fabio@gmail.com>
2024-06-28 00:54:32 +0200

    Implementation of lqry function

--- inst/lqry.m
+++ inst/lqry.m
@@ -1,45 +1,35 @@
-## Copyright (C) 2009-2016   Lukas F. Reichlin
+## Copyright (C) 2024 Fabio Di Iorio <diiorio.fabio@gmail.com>
 ##
-## This file is part of LTI Syncope.
+## This file is part of the Control package for GNU Octave.
 ##
-## LTI Syncope is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## LTI Syncope is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with LTI Syncope.  If not, see <http://www.gnu.org/licenses/>.
+## along with Octave; see the file COPYING.  If not,
+## see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{sys}, @var{q}, @var{r})
-## @deftypefnx {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{sys}, @var{q}, @var{r}, @var{s})
-## @deftypefnx {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{a}, @var{b}, @var{q}, @var{r})
-## @deftypefnx {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{a}, @var{b}, @var{q}, @var{r}, @var{s})
-## @deftypefnx {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{a}, @var{b}, @var{q}, @var{r}, @var{[]}, @var{e})
-## @deftypefnx {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{a}, @var{b}, @var{q}, @var{r}, @var{s}, @var{e})
-## Linear-quadratic regulator.
+## @deftypefn {Function File} {[@var{g}, @var{x}, @var{l}] =} lqry (@var{sys}, @var{q}, @var{r}, @var{s})
+## Linear-quadratic regulator with output weighting.
 ##
 ## @strong{Inputs}
 ## @table @var
 ## @item sys
 ## Continuous or discrete-time @acronym{LTI} model (p-by-m, n states).
-## @item a
-## State matrix of continuous-time system (n-by-n).
-## @item b
-## Input matrix of continuous-time system (n-by-m).
 ## @item q
-## State weighting matrix (n-by-n).
+## Outputs weighting matrix (p-by-p).
 ## @item r
 ## Input weighting matrix (m-by-m).
 ## @item s
-## Optional cross term matrix (n-by-m).  If @var{s} is not specified, a zero matrix is assumed.
-## @item e
-## Optional descriptor matrix (n-by-n).  If @var{e} is not specified, an identity matrix is assumed.
+## Optional cross term matrix (p-by-m).  If @var{s} is not specified, a zero matrix is assumed.
 ## @end table
 ##
 ## @strong{Outputs}
@@ -57,42 +47,50 @@
 ## @group
 ## .
 ## x = A x + B u,   x(0) = x0
+## y = C x + D u
 ##
 ##         inf
-## J(x0) = INT (x' Q x  +  u' R u  +  2 x' S u)  dt
+## J(x0) = INT (y' Q y  +  u' R u  +  2 y' S u)  dt
 ##          0
 ##
 ## L = eig (A - B*G)
 ## @end group
 ## @end example
-## @seealso{care, dare, dlqr}
+## @seealso{lqr, dare, dlqr}
 ## @end deftypefn
 
-## Author: Lukas Reichlin <lukas.reichlin@gmail.com>
-## Created: November 2009
-## Version: 0.2
+## Author: Fabio Di Iorio <diiorio.fabio@gmail.com>
+## Created: June 2024
+## Version: 0.1
 
-function [g, x, l] = lqry (a, b, q, r = [], s = [], e = [])
+function [g, x, l] = lqry (sys, q, r, s = [])
 
-  if (nargin < 3 || nargin > 6)
+  if (nargin < 3 || nargin > 4 || ~isa (sys, "lti"))
     print_usage ();
   endif
 
-  if (isa (a, "lti"))
-    s = r;
-    r = q;
-    q = b;
-    [a, b, c, d, e, tsam] = dssdata (a, []);
-  elseif (nargin < 4)
-    print_usage ();
-  else
-    tsam = 0;
-  endif
+Q = q;
+R = r;
+N = s;
 
-  if (issample (tsam, -1))
-    [x, l, g] = dare (a, b, q, r, s, e);
-  else
-    [x, l, g] = care (a, b, q, r, s, e);
-  endif
+[n, m] = size(sys.b);
+[p, ~] = size(sys.c);
+
+if isempty(N)
+  N=zeros(p,m);
+endif
+
+[A, B, C, D, Ts] = ssdata(sys);
+
+[g, x, l] = lqr (sys, C'*Q*C,D'*Q*D+D'*N+N'*D+R,C'*N+C'*Q*D);
 
 endfunction
+
+%!test
+%! A = [1 -1; 0 -5];
+%! B = [0;1];
+%! C = [1 0];
+%! D = 0
+%! sys = ss(A,B,C,D);
+%! g = lqry(sys,10,1,1);
+%! assert(eig(A-B*g)<0,'lqry error')
