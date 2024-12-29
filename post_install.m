ffe6e500e4dceef7e167a5c4b50591536e8581c5
Torsten Lilge <ttl-octave@mailbox.org>
2024-12-29 22:39:29 +0100

    post-install: add hint on restarting octave

--- post_install.m
+++ post_install.m
@@ -1,8 +1,15 @@
 function post_install (d)
 
+  fprintf ('\n');
   fprintf ('The control package was installed into the directory\n');
   fprintf ('%s.\n', d.dir);
   fprintf ('License and copyright information can be found in ');
   fprintf ('\"packinfo/COPYING\"\n');
+
+  fprintf ('\n');
+  fprintf ('If the control package was updated in the same Octave\n');
+  fprintf ('session, you might have to restart Octave.\n');
+
+  fprintf ('\n');
 
 end
