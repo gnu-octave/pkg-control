d22bae56720f577752ea8696201178df1c7ef6dd
Torsten Lilge <ttl-octave@mailbox.org>
2025-08-24 20:29:01 +0200

    remove obsolete OV_ISNUMERIC macros

--- src/is_vector.cc
+++ src/is_vector.cc
@@ -50,7 +50,7 @@ Avoid nasty stuff like @code{true = isreal (\"a\")}\n\
         for (octave_idx_type i = 0; i < nargin; i++)
         {
             if (args(i).ndims () != 2 || ! (args(i).rows () == 1 || args(i).columns () == 1)
-                || ! args(i).OV_ISNUMERIC ())
+                || ! args(i).isnumeric ())
             {
                 retval = false;
                 break;
