5447b5ca12499ab86bc7c10ff638c72edd7e313a
Torsten Lilge <ttl-octave@mailbox.org>
2025-11-16 22:22:31 +0100

    updated post install message

--- post_install.m
+++ post_install.m
@@ -1,11 +1,13 @@
 function post_install (d)
 
   fprintf ('\n');
-  fprintf ('The control package was installed into the directory\n');
+  fprintf ('The control package was installed into the directory <ID>:\n');
   fprintf ('%s.\n', d.dir);
-  fprintf ('License and copyright information can be found in ');
-  fprintf ('\"packinfo/COPYING\".\n');
-
+  fprintf ('  - License and copyright: \"<ID>/packinfo/COPYING\"\n');
+  fprintf ('  - Documentation:\n');
+  fprintf ('    - PDF:    \"<ID>/doc/control.pdf\"\n');
+  fprintf ('    - QtHelp: Octave GUI documentation browser\n');
+  fprintf ('    - Online: https://gnu-octave.github.io/pkg-control\n');
   fprintf ('\n');
   fprintf ('If the control package was updated in this Octave\n');
   fprintf ('session, you might have to restart Octave.\n');
