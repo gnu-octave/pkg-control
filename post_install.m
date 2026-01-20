ccbae56c75ff08413d25ede02baccc4b003bac0f
Torsten Lilge <ttl-octave@mailbox.org>
2026-01-20 06:43:57 +0100

    NEWS: added hint that bug in frequency response was introduced in 4.2.0

--- post_install.m
+++ post_install.m
@@ -3,9 +3,9 @@ function post_install (d)
   fprintf ('\n');
   fprintf ('The control package was installed into the directory <ID>:\n');
   fprintf ('%s.\n', d.dir);
-  fprintf ('  - License and copyright: \"<ID>/packinfo/COPYING\"\n');
+  fprintf ('  - License and copyright: <ID>/packinfo/COPYING\n');
   fprintf ('  - Documentation:\n');
-  fprintf ('    - PDF:    \"<ID>/doc/control.pdf\"\n');
+  fprintf ('    - PDF:    <ID>/doc/control.pdf\n');
   fprintf ('    - QtHelp: Octave GUI documentation browser\n');
   fprintf ('    - Online: https://gnu-octave.github.io/pkg-control\n');
   fprintf ('\n');
