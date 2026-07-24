e801a6c227d20bb79708e6450106df25cbc0f1fd
Torsten Lilge <ttl-octave@mailbox.org>
2026-07-24 22:28:20 +0200

    replace 'Octave' by 'this program' in licnese informaiton of some files

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -26,18 +26,18 @@ The **control** package is distributed under [GNU General Public License (GPL)](
 ##
 ## This file is part of the control package for GNU Octave.
 ##
-## Octave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of
+## the License, or (at your option) any later version.
 ##
-## Octave is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+## See the GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not,
+## along with this program; see the file COPYING.  If not,
 ## see <http://www.gnu.org/licenses/>.
 
 ```
--- inst/doc_control.m
+++ inst/doc_control.m
@@ -1,19 +1,20 @@
 ## Copyright (C) 2023-2024 Torsten Lilge
 ##
-## This file is part of the Control package for GNU Octave
+## This file is part of the control package for GNU Octave.
 ##
-## This is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of
+## the License, or (at your option) any later version.
 ##
-## This software is distributed in the hope that it will be useful,
+## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+## See the GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with the Control package. If not, see <http://www.gnu.org/licenses/>.
+## along with this program; see the file COPYING.  If not,
+## see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @deftypefn  {} doc_control @var{fcn1} @var{fcn2}  ...
